eaacef018d09d2ede7da77f7f58bcb92991918c0
about8090 <328128521@qq.com>
2019-06-25 02:03:10 +0000

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '关于我们')
+@section('content')
+<h1>关于我们的故事</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '首页')
+@section('content')
+  <h1>微博主页</h1>
+@stop
